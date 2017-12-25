918fa490b76bc9c28772e0c8df613e7089a51620
IF <qq83659105@163.com>
2017-12-25 13:36:46 +0800

    xiug

---
+++ Example/ATest/AViewController.m
@@ -0,0 +1,29 @@
+//
+//  AViewController.m
+//  ATest
+//
+//  Created by qq83659105 on 12/25/2017.
+//  Copyright (c) 2017 qq83659105. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
